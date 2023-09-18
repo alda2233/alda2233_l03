5bc2a14695a3f3d86265811bc67895e493f14384
mohamedaldakhil <mohamedaldakhil@10.84.99.243>
2023-09-18 13:20:41 -0400

    a

---
+++ alda2233_l03/src/t04.py
@@ -0,0 +1,36 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Mohamed Aldakhil
+ID:      169072233
+Email:   alda2233@mylaurier.ca
+__updated__ = "2023-09-18"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+
+def func():
+    """
+    -------------------------------------------------------
+    description
+    Use: 
+    -------------------------------------------------------
+    Parameters:
+        name - description (type)
+    Returns:
+        name - description (type)
+    ------------------------------------------------------
+    """
+
+
+number = float(input("Enter number:"))
+percent = float(input("Enter percent:"))
+
+Calc = number/100*(percent)
+
+
+print(f"a {percent} percent discount on {number} is {Calc:.1f}")
